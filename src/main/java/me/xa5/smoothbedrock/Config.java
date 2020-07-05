722a8da8871ed25e6de869ef3de8cd609d2e75a7
blake <blakeredenius@gmail.com>
2020-07-05 10:42:47 +1000

    Update to 1.16.1 and more reflect example mods build.gradle.

--- src/main/java/me/xa5/smoothbedrock/Config.java
+++ src/main/java/me/xa5/smoothbedrock/Config.java
@@ -1,26 +1,16 @@
 package me.xa5.smoothbedrock;
 
 import blue.endless.jankson.Comment;
-import com.google.common.collect.Lists;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.dimension.DimensionType;
 
-import java.util.List;
-
 public class Config {
-    @Comment("Set to false to make the dimension whitelist act as a blacklist.")
-    public boolean isWhitelist = true;
+    @Comment("true: Dimensions in filter will have flat bedrock. false: Dimensions in list will not have flat bedrock")
+    public boolean isWhitelist = false;
 
-    @Comment("A list of dimension ids that this mod should filter based on the 'Act as whitelist' setting." +
-            "\nModded dimensionFilter will need to be added here to be affected.")
-    public List<String> dimensionFilter = getDefaultFilter();
+    @Comment("A list of dimension ids that this mod should filter based on the 'Act as whitelist' setting.")
+    public String[] dimensionFilter = getDefaultFilter();
 
-    private List<String> getDefaultFilter() {
-        return Lists.newArrayList(getDimId(DimensionType.OVERWORLD).toString(), getDimId(DimensionType.THE_NETHER).toString());
-    }
-
-    private static Identifier getDimId(DimensionType type) {
-        return Registry.DIMENSION.getId(type);
+    private String[] getDefaultFilter() {
+        return new String[]{DimensionType.THE_END_REGISTRY_KEY.getValue().toString()};
     }
 }
